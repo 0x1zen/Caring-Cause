334aea0be5779009bfa1a66155e1459314a65f7e
0x1zen <rajdubalwork@gmail.com>
2024-03-12 12:06:46 +0530

    WIP:Added db.js file for mongoose schema

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+
+
+
+  module.exports = {
+
+};
